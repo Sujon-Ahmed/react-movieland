a4c3bfc089f2f220d845143a9f6a6bcc94acb80c
Sujon Ahmed <sujonahmed5284@gmail.com>
2023-08-22 21:56:34 +0600

    updated https access

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
